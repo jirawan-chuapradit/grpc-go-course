9b009f5b3eec2d7851145cb5080c9a7f1c67be91
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2021-04-17 15:03:42 +0700

    Create greet client

---
+++ greet/greet_client/client.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jirawan-chuapradit/grpc-go-course/greet/greetpb"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main () {
+
+	fmt.Println("Hello I'm a client.")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer  cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created client: %f", c)
+
+}
